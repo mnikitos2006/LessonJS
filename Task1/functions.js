2afb51671164fdbbcdac9cc2b856299d15e290d9
Nikita <mnikitos2006@gmail.com>
2023-01-11 14:03:11 -0300

    решение задач

--- Task1/functions.js
+++ Task1/functions.js
@@ -10,8 +10,64 @@ function getSeconds(hours){
     return hours*60*60
 
 }
+//Задача6
+// let age="16"
+// console.log("Мне "+age+" лет")
+function age(age){
+    return "Мне " + age + "лет"
+}
+
+//Задача7
+
+// let a=1
+// let b=a*60*60
+// console.log(b+"-количество секунд в часе")
+//
+// let c=1
+// let d=c*60*60*24
+// console.log(d+"-количество секунд в сутках")
+//
+// let e=1
+// let j=e*60*60*24*31
+// console.log(j+"-количество секунд в месяце")
+
+function sekh(hours){
+    return hours*60*60
+}
+function sekd(hours){
+    return hours*60*60*24
+}
+function sekw(hours){
+    return hours*60*60*24*31
+}
+
+//Задача8
+// let hour = "10"
+// let minute = "10"
+// let seconds = "10"
+// console.log(hour+":"+minute+":"+seconds)
+function vremya(hour,minute,seconds){
+    return hour+":"+minute+":"+seconds
+}
 
 
+//Задача5
+// let name="Никита!"
+// console.log("Привет,"+name)
+function name(name){
+    return "Привет," + name;
+}
+
+
+
+//Задача4
+// var str1='Привет,'
+// var str2='Мир!.'
+// console.log(str1+str2)
+
+function strgg(str1, str2){
+    return str1 + str2;
+}
 
 //Задача2
 // let a=10
@@ -37,4 +93,13 @@ function delenie(a,b){
     }
     return a/b
 }
-module.exports = {sum,proizved,minus,delenie,getSeconds};
+module.exports = {sum,proizved,minus,delenie,getSeconds,strgg,name,age,sekh,sekd,sekw,vremya};
+
+
+
+
+
+
+
+
+
